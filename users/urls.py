197427d04e185707d3454debaccb2f9522018f99
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-04-25 19:16:18 +0300

    Done

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
-from rest_framework import routers
 
+from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
 from users.views.payment import PaymentViewSet
 from users.views.users import UserViewSet
 
 app_name = UsersConfig.name
 
-course_router = routers.SimpleRouter()
+course_router = DefaultRouter()
 course_router.register(r'user', UserViewSet, basename='user')
 course_router.register(r'payment', PaymentViewSet, basename='payment')
 
-urlpatterns = [] + course_router.urls
+urlpatterns = [
+
+              ] + course_router.urls
