597d3f9287ac99bf8d2d9e3dd3bed5640dc0fb80
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-05-12 23:46:27 +0300

    Done 2

--- materials/views/course.py
+++ materials/views/course.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course
+from materials.models import Course, CourseSubscription
 from materials.paginators import LessonCoursePaginator
 from materials.serializers.course import CourseSerializer
+from materials.tasks import send_course_update
 from users.permissions import IsModerStaff, IsOwnerStaff
 import stripe
 
@@ -34,3 +35,18 @@ class CourseViewSet(ModelViewSet):
     def perform_create(self, serializer):
         user = serializer.save(user=self.request.user)
         user.save()
+
+    def update(self, request, pk):
+        course = self.get_object()
+        serializer = self.get_serializer(course, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        subscribers = CourseSubscription.objects.filter(course=course.pk)
+        email = [subscriber.user.email for subscriber in subscribers]
+        send_course_update.delay(
+            course_id=course.id,
+            subject=f"Курс {course.name} был обновлен",
+            message="Быстрее посмотрите что там нового",
+            recipient_list=email
+        )
+        return Response(serializer.data)
